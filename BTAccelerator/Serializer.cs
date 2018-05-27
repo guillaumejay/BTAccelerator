7a69035a40564567f7216a9709476597863d529f
Guillaume Jay <guillaumejay@users.noreply.github.com>
2018-05-27 10:58:53 +0200

    Added multiple folder locations for movement update

---
+++ BTAccelerator/Serializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BTAccelerator
+{
+    /// <summary>
+    /// https://stackoverflow.com/a/2347661
+    /// </summary>
+    public static class Serializer
+    {
+ 
+        public static string XmlSerializeToString(this object objectInstance)
+        {
+            var serializer = new XmlSerializer(objectInstance.GetType());
+            var sb = new StringBuilder();
+
+            using (TextWriter writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, objectInstance);
+            }
+
+            return sb.ToString();
+        }
+
+        public static T XmlDeserializeFromString<T>(this string objectData)
+        {
+            return (T)XmlDeserializeFromString(objectData, typeof(T));
+        }
+
+        /// <param name="objectData"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object XmlDeserializeFromString(this string objectData, Type type)
+        {
+            var serializer = new XmlSerializer(type);
+            object result;
+
+            using (TextReader reader = new StringReader(objectData))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+    }
+}
